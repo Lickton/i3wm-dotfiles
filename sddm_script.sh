a609e4f8ba8cbcd41d40b4a29bcd10bdc2c2250d
Lickton <lilihuanmao@gmail.com>
2023-07-19 16:34:22 +0800

    X11 Settings

--- sddm_script.sh
+++ sddm_script.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
+# @/etc/X11/xorg.conf.d/sddm_script.sh
 
 intern=eDP-1-1
-extern=DP-0
+extern0=DP-0
+extern1=HDMI-0
 
-if xrandr | grep "$extern disconnected"; then
-    xrandr --output "$extern" --off --output "$intern" --auto
+if xrandr | grep "$extern0 disconnected"; then
+    xrandr --output "$extern0" --off --output "$intern" --auto
 else
-    xrandr --output "$intern" --off --output "$extern" --auto
+    xrandr --output "$intern" --off --output "$extern0" --auto
 fi
